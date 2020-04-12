a79d3cee6e98ceccd61fb4b874169955167ac92c
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2020-04-12 14:11:51 +0900

    reconfig

--- _posts/migrate.rb
+++ _posts/migrate.rb
@@ -1,1 +1,17 @@
 # migrate image tags
+
+["*markdown", "*md"].each do |r|
+  Dir.glob(r).each do |fname|
+    string = File.read(fname)
+
+    string.each_line do |line|
+      puts line
+      if line.match(/\{\%img\s+(\w+)\s+\'(.*)\'\%\}/)
+      end
+    end
+  end
+end
+
+
+
+
